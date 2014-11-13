da460c14835c92550392b58dd0e0ac9a26483235
Graeme Gott <graeme@gottcode.org>
2014-11-13 13:56:45 -0500

    Update translations.

---
+++ translations/gottet_cs.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../src/board.cpp" line="89"/>
+        <source>Question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="89"/>
+        <source>End the current game?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="175"/>
+        <source>&lt;big&gt;&lt;b&gt;Paused&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to resume playing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="403"/>
+        <source>&lt;big&gt;&lt;b&gt;Game Over!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to start a new game.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="50"/>
+        <source>Select application language:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="53"/>
+        <source>&lt;System Language&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoard</name>
+    <message>
+        <location filename="../src/score_board.cpp" line="33"/>
+        <source>Gottet Scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="37"/>
+        <source>Level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="37"/>
+        <source>Lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="37"/>
+        <source>Score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="56"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../src/window.cpp" line="44"/>
+        <location filename="../src/window.cpp" line="224"/>
+        <source>Gottet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="93"/>
+        <source>Click to start a new game.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="111"/>
+        <source>&amp;Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="112"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="113"/>
+        <source>&amp;Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="113"/>
+        <location filename="../src/window.cpp" line="115"/>
+        <source>P</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="115"/>
+        <source>&amp;Resume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="118"/>
+        <source>&amp;Scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="120"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="123"/>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="124"/>
+        <source>Application &amp;Language...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="126"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="127"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="129"/>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="144"/>
+        <source>Next Piece</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="146"/>
+        <source>Level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="148"/>
+        <source>Removed Lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="150"/>
+        <source>Score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="223"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="225"/>
+        <source>A tetris clone I made for my wife</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="226"/>
+        <source>Copyright &amp;copy; 2008-%1 Graeme Gott.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="227"/>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/gottet_zh_TW.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../src/board.cpp" line="89"/>
+        <source>Question</source>
+        <translation>問題</translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="89"/>
+        <source>End the current game?</source>
+        <translation>結束目前的遊戲？</translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="175"/>
+        <source>&lt;big&gt;&lt;b&gt;Paused&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to resume playing.</source>
+        <translation>&lt;big&gt;&lt;b&gt;已暫停&lt;/b&gt;&lt;/big&gt;&lt;br&gt;點擊以繼續進行遊戲。</translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="403"/>
+        <source>&lt;big&gt;&lt;b&gt;Game Over!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to start a new game.</source>
+        <translation>&lt;big&gt;&lt;b&gt;遊戲結束！&lt;/b&gt;&lt;/big&gt;&lt;br&gt;點擊以開啟一個新遊戲。</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="50"/>
+        <source>Select application language:</source>
+        <translation>選擇應用程式語言：</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="53"/>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;系統語言&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Note</source>
+        <translation>註記</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>請重新啟動此應用程式以使語言的更動生效。</translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoard</name>
+    <message>
+        <location filename="../src/score_board.cpp" line="33"/>
+        <source>Gottet Scores</source>
+        <translation>Gottet 分數</translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="37"/>
+        <source>Level</source>
+        <translation>等級</translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="37"/>
+        <source>Lines</source>
+        <translation>行數</translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="37"/>
+        <source>Score</source>
+        <translation>分數</translation>
+    </message>
+    <message>
+        <location filename="../src/score_board.cpp" line="56"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../src/window.cpp" line="44"/>
+        <location filename="../src/window.cpp" line="224"/>
+        <source>Gottet</source>
+        <translation>Gottet</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="93"/>
+        <source>Click to start a new game.</source>
+        <translation>點擊以開啟一個新遊戲。</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="111"/>
+        <source>&amp;Game</source>
+        <translation>遊戲(&amp;G)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="112"/>
+        <source>&amp;New</source>
+        <translation>新遊戲(&amp;N)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="113"/>
+        <source>&amp;Pause</source>
+        <translation>暫停(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="113"/>
+        <location filename="../src/window.cpp" line="115"/>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="115"/>
+        <source>&amp;Resume</source>
+        <translation>繼續(&amp;R)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="118"/>
+        <source>&amp;Scores</source>
+        <translation>分數(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="120"/>
+        <source>&amp;Quit</source>
+        <translation>離開(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="123"/>
+        <source>&amp;Settings</source>
+        <translation>設定(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="124"/>
+        <source>Application &amp;Language...</source>
+        <translation>應用程式語言...(&amp;L)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="126"/>
+        <source>&amp;Help</source>
+        <translation>求助(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="127"/>
+        <source>&amp;About</source>
+        <translation>關於(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="129"/>
+        <source>About &amp;Qt</source>
+        <translation>關於 Qt(&amp;Q)</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="144"/>
+        <source>Next Piece</source>
+        <translation>下一個</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="146"/>
+        <source>Level</source>
+        <translation>等級</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="148"/>
+        <source>Removed Lines</source>
+        <translation>已移除的行數</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="150"/>
+        <source>Score</source>
+        <translation>分數</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="223"/>
+        <source>About</source>
+        <translation>關於</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="225"/>
+        <source>A tetris clone I made for my wife</source>
+        <translation>獻給我妻子的一個俄羅斯方塊克隆</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="226"/>
+        <source>Copyright &amp;copy; 2008-%1 Graeme Gott.</source>
+        <translation>Copyright &amp;copy; 2008-%1 Graeme Gott.</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="227"/>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>以 &lt;a href=%1&gt;GPL 3&lt;/a&gt; 授權條款釋出</translation>
+    </message>
+</context>
+</TS>
