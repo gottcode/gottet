2ae1bf9d75581ad58982deb21f1d2b47980821be
Graeme Gott <graeme@gottcode.org>
2014-11-14 15:47:29 -0500

    Update appdata file.

--- icons/po/update-po.sh
+++ icons/po/update-po.sh
@@ -21,10 +21,13 @@ rm -f gottet.appdata.xml.in
 cp gottet.appdata.xml gottet.appdata.xml.in
 sed -e '/p xml:lang/ d' \
 	-e '/summary xml:lang/ d' \
+	-e '/name xml:lang/ d' \
 	-e 's/<p>/<_p>/' \
 	-e 's/<\/p>/<\/_p>/' \
 	-e 's/<summary>/<_summary>/' \
 	-e 's/<\/summary>/<\/_summary>/' \
+	-e 's/<name>/<_name>/' \
+	-e 's/<\/name>/<\/_name>/' \
 	-i gottet.appdata.xml.in
 intltool-extract --quiet --type=gettext/xml gottet.appdata.xml.in
 
