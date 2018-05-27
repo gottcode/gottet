4ebfefa13e26881a336397d923ba45cd9f9f2b95
Graeme Gott <graeme@gottcode.org>
2018-05-27 07:22:43 -0400

    Sync translations with Transifex.

---
+++ translations/gottet_pt.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Question</source>
+        <translation>Questão</translation>
+    </message>
+    <message>
+        <source>End the current game?</source>
+        <translation>Terminar o jogo atual?</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Paused&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to resume playing.</source>
+        <translation>&lt;big&gt;&lt;b&gt;Pausado&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Clique para continuar o jogo.</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Game Over!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to start a new game.</source>
+        <translation>&lt;big&gt;&lt;b&gt;Fim do Jogo!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Clique para começar um novo jogo.</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Selecione o idioma da aplicação:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;Idioma do Sistema&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Nota</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Por favor reinicie a aplicação para a alteração ao idioma surtir efeito.</translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoard</name>
+    <message>
+        <source>Gottet Scores</source>
+        <translation>Pontuações do Gottet</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Nível</translation>
+    </message>
+    <message>
+        <source>Lines</source>
+        <translation>Linhas</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Pontuação</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Gottet</source>
+        <translation>Gottet</translation>
+    </message>
+    <message>
+        <source>Click to start a new game.</source>
+        <translation>Clique para começar um novo jogo</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Jogo</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Novo</translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation>&amp;Pausa</translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <source>&amp;Resume</source>
+        <translation>&amp;Continuar</translation>
+    </message>
+    <message>
+        <source>&amp;Scores</source>
+        <translation>P&amp;ontuações</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Sair</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Definições</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>&amp;Idioma da Aplicação</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Ajuda</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>So&amp;bre</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Sobre o &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Next Piece</source>
+        <translation>Próxima Peça</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Nível</translation>
+    </message>
+    <message>
+        <source>Removed Lines</source>
+        <translation>Linhas Removidas</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Pontuação</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Sobre</translation>
+    </message>
+    <message>
+        <source>A tetris clone I made for my wife</source>
+        <translation>Um clone do Tetris que fiz para a minha esposa</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2008-%1 Graeme Gott.</source>
+        <translation>Direitos de autor &amp;copy; 2008-%1 Graeme Gott.</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Lançado sob a licença &lt;a href=%1&gt;GPL 3&lt;/a&gt;</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/gottet_zh.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Question</source>
+        <translation>问题</translation>
+    </message>
+    <message>
+        <source>End the current game?</source>
+        <translation>结束当前游戏？</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Paused&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to resume playing.</source>
+        <translation>&lt;big&gt;&lt;b&gt;已暂停&lt;/b&gt;&lt;/big&gt;&lt;br&gt;点击以恢复游戏。</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Game Over!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to start a new game.</source>
+        <translation>&lt;big&gt;&lt;b&gt;游戏结束！&lt;/b&gt;&lt;/big&gt;&lt;br&gt;点击以开始新游戏。</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>选择本游戏的语言:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;系统语言&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>提示</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>请重启本程序使语言设置生效。</translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoard</name>
+    <message>
+        <source>Gottet Scores</source>
+        <translation>Gottet 分数</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>等级</translation>
+    </message>
+    <message>
+        <source>Lines</source>
+        <translation>行数</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>分数</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>好</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Gottet</source>
+        <translation>Gottet</translation>
+    </message>
+    <message>
+        <source>Click to start a new game.</source>
+        <translation>点击以开始新游戏</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;游戏</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;新游戏</translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation>&amp;暂停</translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <source>&amp;Resume</source>
+        <translation>&amp;恢复</translation>
+    </message>
+    <message>
+        <source>&amp;Scores</source>
+        <translation>&amp;分数</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;退出程序</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;设置</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>程序 &amp;语言...</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;帮助</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;关于</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>关于 &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Next Piece</source>
+        <translation>下一个方块</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>等级</translation>
+    </message>
+    <message>
+        <source>Removed Lines</source>
+        <translation>消除行数</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>分数</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>A tetris clone I made for my wife</source>
+        <translation>我为我妻子打造的俄罗斯方块克隆</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2008-%1 Graeme Gott.</source>
+        <translation>版权所有 &amp;copy; 2008-%1 Graeme Gott.</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>基于 &lt;a href=%1&gt;GPL 3&lt;/a&gt; 协议发布</translation>
+    </message>
+</context>
+</TS>
