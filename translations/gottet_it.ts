6ade445889de9beeb073a488a297144b7236e195
Hyd3L <hyd3l@gmx.com>
2020-04-25 22:40:51 +0200

    Added Italian translation.

---
+++ translations/gottet_it.ts
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Question</source>
+        <translation>Domanda</translation>
+    </message>
+    <message>
+        <source>End the current game?</source>
+        <translation>Terminare la partita in corso?</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Paused&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to resume playing.</source>
+        <translation>&lt;big&gt;&lt;b&gt;In pausa&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Clicca per continuare la partita.</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Game Over!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to start a new game.</source>
+        <translation>&lt;big&gt;&lt;b&gt;Fine partita!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Clicca per iniziare una nuova partita.</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Seleziona la lingua dell'&apos;applicazione:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;Lingua di Sistema&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Nota</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Prego, riavviare l&apos;applicazione per rendere effettiva la modifica della lingua.</translation>
+    </message>
+</context>
+<context>
+    <name>ScoreBoard</name>
+    <message>
+        <source>Gottet Scores</source>
+        <translation>Punteggi di Gottet</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Livello</translation>
+    </message>
+    <message>
+        <source>Lines</source>
+        <translation>Linee</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Punteggio</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Gottet</source>
+        <translation>Gottet</translation>
+    </message>
+    <message>
+        <source>Click to start a new game.</source>
+        <translation>Clicca per iniziare una nuova partita.</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Partita</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Nuova</translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation>&amp;Pausa</translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <source>&amp;Resume</source>
+        <translation>&amp;Riprendi</translation>
+    </message>
+    <message>
+        <source>&amp;Scores</source>
+        <translation>P&amp;unteggi</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Esci</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Impostazioni</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>&amp;Lingua dell&apos;applicazione…</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Aiuto</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Informazioni su Gottet</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Informazioni su &amp;Qt</translation>
+    </message>
+    <message>
+        <source>Next Piece</source>
+        <translation>Prossimo pezzo</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Livello</translation>
+    </message>
+    <message>
+        <source>Removed Lines</source>
+        <translation>Linee completate</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Punteggio</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Informazioni su Gottet</translation>
+    </message>
+    <message>
+        <source>A tetris clone I made for my wife</source>
+        <translation>Un clone di Tetris che ho fatto per mia moglie</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2008-%1 Graeme Gott.</source>
+        <translation>Copyright &amp;copy; 2008-%1 Graeme Gott.</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Rilasciato sotto licenza &lt;a href=%1&gt;GPL 3&lt;/a&gt;</translation>
+    </message>
+</context>
+</TS>
