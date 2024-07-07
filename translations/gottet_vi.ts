224a469835cba5a5fd29e6dd41913f1e7921f2b1
Graeme Gott <graeme@gottcode.org>
2024-07-07 01:50:12 -0400

    Update translation language to be en

--- translations/gottet_vi.ts
+++ translations/gottet_vi.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="vi">
+<TS version="2.1" language="vi" sourcelanguage="en">
 <context>
     <name>Board</name>
     <message>
