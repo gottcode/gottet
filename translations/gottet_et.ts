308419fffc5ccc4cab370a2ae8817639d57bc26f
Graeme Gott <graeme@gottcode.org>
2025-01-02 06:26:47 -0500

    Sync translations with Transifex

---
+++ translations/gottet_et.ts
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Question</source>
+        <translation>Küsimus</translation>
+    </message>
+    <message>
+        <source>End the current game?</source>
+        <translation>Kas lõpetame selle mängu?</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Paused&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to resume playing.</source>
+        <translation>&lt;big&gt;&lt;b&gt;Mäng on pausil&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Mängimise jätkamiseks klõpsi.</translation>
+    </message>
+    <message>
+        <source>&lt;big&gt;&lt;b&gt;Game Over!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Click to start a new game.</source>
+        <translation>&lt;big&gt;&lt;b&gt;Mäng on läbi!&lt;/b&gt;&lt;/big&gt;&lt;br&gt;Uue mängu alustamiseks klõpsi.</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Vali rakenduse keel:</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>Süsteemi keel</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Märkus</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Keele vahetamise jõustamiseks palun käivita rakendus uuesti.</translation>
+    </message>
+</context>
+<context>
+    <name>ScoresDialog</name>
+    <message>
+        <source>Level</source>
+        <translation>Tase</translation>
+    </message>
+    <message>
+        <source>Lines</source>
+        <translation>Ridu</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Tulemus</translation>
+    </message>
+    <message>
+        <source>High Scores</source>
+        <translation>Edetabel</translation>
+    </message>
+    <message>
+        <source>Rank</source>
+        <translation>Koht</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <source>#%1</source>
+        <translation>#%1</translation>
+    </message>
+    <message>
+        <source>Congratulations!</source>
+        <translation>Õnnitleme!</translation>
+    </message>
+    <message>
+        <source>You beat your top score!</source>
+        <translation>Tegid senistest tulemustest parema tulemuse!</translation>
+    </message>
+    <message>
+        <source>Well done!</source>
+        <translation>Hea töö!</translation>
+    </message>
+    <message>
+        <source>You have a new high score!</source>
+        <translation>Tegid uue tabelitulemuse!</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>Gottet</source>
+        <translation>Gottet</translation>
+    </message>
+    <message>
+        <source>Click to start a new game.</source>
+        <translation>Uue mängu alustamiseks klõpsi.</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Mäng</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Uus</translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation>&amp;Peata ajutiselt</translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <source>&amp;Scores</source>
+        <translation>&amp;Tulemused</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Välju</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Seadistused</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>Rakenduse &amp;keel...</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Abiteave</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Rakenduse teave</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Teave &amp;Qt kohta</translation>
+    </message>
+    <message>
+        <source>Next Piece</source>
+        <translation>Järgmine klots</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Tase</translation>
+    </message>
+    <message>
+        <source>Removed Lines</source>
+        <translation>Eemaldatud ridu</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Tulemus</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Rakenduse teave</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2008-%1 Graeme Gott.</source>
+        <translation>Autoriõigused &amp;copy; 2008-%1 Graeme Gott.</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Avaldatud &lt;a href=%1&gt;GPL 3&lt;/a&gt; litsentsi alusel</translation>
+    </message>
+    <message>
+        <source>Ctrl+H</source>
+        <translation>Ctrl+H</translation>
+    </message>
+    <message>
+        <source>A simple falling blocks game</source>
+        <translation>Lihtne klotside kukutamise mäng</translation>
+    </message>
+</context>
+</TS>
